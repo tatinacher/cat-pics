0be123577731d3d9e313f662e2c4040433b66908
Tatina Cherednichenko <tatinacher@gmail.com>
2019-06-07 17:41:31 +0300

    Update README.md. Change footer.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CatsPics
-React JS application
+
+CatPics is an application for searching cat pictures and cat breeds made with ReactJS + Redux.
+
+You can use CatPics as an anonymos user or as an authorized user as well. 
+Authorized user has access to "Favourite" page and can add cat images to Favorite page.
+
+"Main Page" contains two components: Search component and Random image component.
+Search component helps user to search info about cat breed.
+
+"Explore" page shows list of images. User can sort images or filter images by type.
+
+"Favourite" page can be seen only by authorized user. It containts list of images that user saved in this application.
+
+User can add image to favorite page by clicking a star icon in the right upper corner of particular image. Also user can copy image link to clipboard by clicking copy icon on image.
+
+API used: thecatapi.com
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Footer extends Component {
   constructor(props){
@@ -12,9 +13,15 @@ class Footer extends Component {
     return (
       <div className="hero-foot">
         <div className="container">
-          <span>Thanks to thecatapi.com</span>
-          <span>Photo by Kasya Shahovskaya on Unsplash</span>
-          <span className="is-pulled-right">Made in 2019 by Tatiana Cherednichenko</span>
+          <div class="columns">
+            <div class="column">
+              <span>Thanks to <Link to="thecatapi.com">thecatapi.com</Link></span>
+              <span>Photo by Kasya Shahovskaya on Unsplash</span>
+            </div>
+            <div class="column">
+              <span className="column is-pulled-right">Made in 2019 by Tatiana Cherednichenko</span>
+            </div>
+          </div>
         </div>
       </div>
     );
